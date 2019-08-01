d9a0ba472032b449c8a6012cec43bb9f4eaa4bb3
alovn <alovn@live.com>
2019-08-01 23:59:32 +0800

    feat: reflection

--- golang/grpc_demo/grpc_hello/server.go
+++ golang/grpc_demo/grpc_hello/server.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"log"
 	"net"
-
+	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/peer"
+	"google.golang.org/grpc/reflection"
 	pb "grpc_demo/grpc_hello/hello"
 )
 
@@ -19,7 +21,24 @@ type server struct{}
 // SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
 	log.Printf("Received: %v", in.Greeting)
+
+	getClietIP(ctx)
+
 	return &pb.HelloResponse{Reply: "Hello " + in.Greeting, Number: []int32{1,2}}, nil
+}
+
+func getClietIP(ctx context.Context) (string, error) {
+	pr, ok := peer.FromContext(ctx)
+	if !ok {
+		return "", fmt.Errorf("getClinetIP, invoke FromContext() failed")
+	}
+	if pr.Addr == net.Addr(nil) {
+		return "", fmt.Errorf("getClientIP, peer.Addr is nil")
+	}
+	log.Println(pr.Addr.String())
+	return pr.Addr.String(), nil
+	// addSlice := strings.Split(pr.Addr.String(), ":")
+	//return addSlice[0], nil
 }
 
 func main() {
@@ -29,6 +48,9 @@ func main() {
 	}
 	s := grpc.NewServer()
 	pb.RegisterHelloServiceServer(s, &server{})
+
+	reflection.Register(s)
+	
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
