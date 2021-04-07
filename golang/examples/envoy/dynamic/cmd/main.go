771b02eb0caa8e4b1d7428381f98b239b2a6ec1a
alovn <alovn@live.com>
2021-04-07 12:35:04 +0800

    feat: envoy dynamic忘记提交

---
+++ golang/examples/envoy/dynamic/cmd/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+
+	cachev3 "github.com/envoyproxy/go-control-plane/pkg/cache/v3"
+	serverv3 "github.com/envoyproxy/go-control-plane/pkg/server/v3"
+	testv3 "github.com/envoyproxy/go-control-plane/pkg/test/v3"
+
+	"example"
+)
+
+var (
+	l example.Logger
+
+	port     uint
+	basePort uint
+	mode     string
+
+	nodeID string
+)
+
+func init() {
+	l = example.Logger{}
+
+	flag.BoolVar(&l.Debug, "debug", false, "Enable xDS server debug logging")
+
+	// The port that this xDS server listens on
+	flag.UintVar(&port, "port", 18000, "xDS management server port")
+
+	// Tell Envoy to use this Node ID
+	flag.StringVar(&nodeID, "nodeID", "test-id", "Node ID")
+}
+
+func main() {
+	flag.Parse()
+
+	// Create a cache
+	cache := cachev3.NewSnapshotCache(false, cachev3.IDHash{}, l)
+
+	// Create the snapshot that we'll serve to Envoy
+	snapshot := example.GenerateSnapshot()
+	if err := snapshot.Consistent(); err != nil {
+		l.Errorf("snapshot inconsistency: %+v\n%+v", snapshot, err)
+		os.Exit(1)
+	}
+	l.Debugf("will serve snapshot %+v", snapshot)
+
+	// Add the snapshot to the cache
+	if err := cache.SetSnapshot(nodeID, snapshot); err != nil {
+		l.Errorf("snapshot error %q for %+v", err, snapshot)
+		os.Exit(1)
+	}
+
+	// Run the xDS server
+	ctx := context.Background()
+	cb := &testv3.Callbacks{Debug: l.Debug}
+	srv := serverv3.NewServer(ctx, cache, cb)
+	example.RunServer(ctx, srv, port)
+}
---
+++ golang/examples/envoy/dynamic/go.mod
@@ -0,0 +1,9 @@
+module example
+
+go 1.16
+
+require (
+	github.com/envoyproxy/go-control-plane v0.9.9-0.20201210154907-fd9021fe5dad
+	github.com/golang/protobuf v1.5.2
+	google.golang.org/grpc v1.36.1
+)
